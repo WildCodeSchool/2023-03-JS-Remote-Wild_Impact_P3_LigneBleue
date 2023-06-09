4f8a489607613e1123158aa14eed77bd8e768cce
Jumzer <bluevictor@hotmail.fr>
2023-06-09 10:27:17 +0200

    database finalisée create & premiers insert

--- backend/database.sql
+++ backend/database.sql
@@ -9,18 +9,18 @@ CREATE TABLE users (
 
 CREATE TABLE formations (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(100) NOT NULL,
+  title varchar(255) NOT NULL,
   icon varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE quizz (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(100) NOT NULL
+  title varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE tutorials (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
+  name varchar(255) NOT NULL,
   target varchar(255) NOT NULL,
   explanation longtext NOT NULL,
   image longtext,
@@ -41,7 +41,7 @@ CREATE TABLE validated_by_user (
 
 CREATE TABLE resources (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
+  name varchar(255) NOT NULL,
   content longtext NOT NULL,
   tutorial_id INT NOT NULL,
     CONSTRAINT fk_resource_tutorial FOREIGN KEY (tutorial_id) REFERENCES tutorials(id) ON DELETE CASCADE
@@ -49,22 +49,22 @@ CREATE TABLE resources (
 
 CREATE TABLE questions (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  content varchar(255) NOT NULL,
+  content longtext NOT NULL,
   quizz_id INT NOT NULL,
     CONSTRAINT fk_question_quizz FOREIGN KEY (quizz_id) REFERENCES quizz(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE answers (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  content varchar(255) NOT NULL,
+  content longtext NOT NULL,
   question_id INT NOT NULL,
     CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO users(name,mail,password,admin,creation_date) VALUES ('Nina Robinson','nina.robinson@example.com','bethany',false,'01/07/1993');
-INSERT INTO users(name,mail,password,admin,creation_date) VALUES ('Don Bishop','don.bishop@example.com','abraham',false,'03/02/1975');
-INSERT INTO users(name,mail,password,admin,creation_date) VALUES ('Heather Simmmons','heather.simmmons@example.com','575757',true,'09/03/1950');
+INSERT INTO users(name,mail,password,admin,creation_date) VALUES ('Nina Robinson','nina.robinson@example.com','bethany',false,'2021/07/01');
+INSERT INTO users(name,mail,password,admin,creation_date) VALUES ('Don Bishop','don.bishop@example.com','abraham',false,'2021/02/03');
+INSERT INTO users(name,mail,password,admin,creation_date) VALUES ('Heather Simmmons','heather.simmmons@example.com','575757',true,'2021/03/04');
 
 
 INSERT INTO formations(title,icon) VALUES ('Utiliser Ligne bleue','https://img.icons8.com/?size=512&id=13616&format=png%27');
@@ -81,16 +81,46 @@ INSERT INTO formations(title,icon) VALUES ('Rechercher un tuto','https://img.ico
 INSERT INTO formations(title,icon) VALUES ('Pour aller plus loin','https://img.icons8.com/?size=512&id=nWZlu7wJW-Sr&format=png%27');
 
 
-INSERT INTO quizz(title) VALUES ('Vérifiez vos connaissances sur WhatsApp');
-INSERT INTO quizz(title) VALUES ('Vérifiez vos connaissances sur l''utilisation de votre téléphone');
-INSERT INTO quizz(title) VALUES ('Vérifiez vos connaissances sur le visionnage d''une vidéo');
+INSERT INTO quizz(title) VALUES ('Quizz 1 - Vérifiez vos connaissances sur WhatsApp');
+INSERT INTO quizz(title) VALUES ('Quizz 2 - Vérifiez vos connaissances sur l''utilisation de votre téléphone');
+INSERT INTO quizz(title) VALUES ('Quizz 3 - Vérifiez vos connaissances sur le visionnage d''une vidéo');
 
 
-INSERT INTO tutorials(name,target,explanation,published,creation_date,quizz_id,formation_id) VALUES ('Donec tincidunt sodales purus eu','Aenean vitae mauris magna. Pellentesque ultrices nisl orci, eget viverra tellus pharetra vulputate. Quisque dictum.','https://randompicturegenerator.com/img/dragon-generator/g9b72abb3388f87375d88f2c3cb2fa279a0fe380ab82edac115301d9331dfe3b7c3e453d72e99ba931de26c3f1540df1e_640.jpg',true,'14/04/2023',1,1);
-INSERT INTO tutorials(name,target,explanation,published,creation_date,quizz_id,formation_id) VALUES ('Sed ut venenatis nunc. Mauris.','Ut dignissim at nulla vel rhoncus. Sed vitae felis sit amet quam consectetur congue. Morbi','https://randompicturegenerator.com/img/dragon-generator/g9d319c882c20258e422f2e608f9cb4f132faccc2c5d0d8f642b331e75e59815d87c2cba770a8ce77fce4afdacaacc782_640.jpg',true,'04/05/2023',3,2);
-INSERT INTO tutorials(name,target,explanation,published,creation_date,quizz_id,formation_id) VALUES ('Vestibulum semper et lorem sed','Etiam ut sapien quam. Proin lacus leo, elementum in volutpat at, pulvinar sit amet urna.','https://randompicturegenerator.com/img/dragon-generator/g43a4ef235a57ff339250b27e486be537454a43ec5fb652c2cdc99754019d679e232a4785776983bc39a755e687f362a3_640.jpg',false,'17/06/2023',2,3);
+INSERT INTO tutorials(name,target,explanation,image, published,creation_date,quizz_id,formation_id) 
+VALUES ('Tuto 1','Aenean vitae mauris magna. Pellentesque ultrices nisl orci, eget viverra tellus pharetra vulputate. Quisque dictum.','Explanation1','https://randompicturegenerator.com/img/dragon-generator/g9b72abb3388f87375d88f2c3cb2fa279a0fe380ab82edac115301d9331dfe3b7c3e453d72e99ba931de26c3f1540df1e_640.jpg',true,'2023/04/14',1,1);
+INSERT INTO tutorials(name,target,explanation,image, published,creation_date,quizz_id,formation_id) VALUES ('Tuto 2','Ut dignissim at nulla vel rhoncus. Sed vitae felis sit amet quam consectetur congue. Morbi','Explanation2','https://randompicturegenerator.com/img/dragon-generator/g9d319c882c20258e422f2e608f9cb4f132faccc2c5d0d8f642b331e75e59815d87c2cba770a8ce77fce4afdacaacc782_640.jpg',true,'2023/04/05',2,2);
+INSERT INTO tutorials(name,target,explanation,image,published,creation_date,quizz_id,formation_id) VALUES ('Tuto 3','Etiam ut sapien quam. Proin lacus leo, elementum in volutpat at, pulvinar sit amet urna.','Explanation3','https://randompicturegenerator.com/img/dragon-generator/g43a4ef235a57ff339250b27e486be537454a43ec5fb652c2cdc99754019d679e232a4785776983bc39a755e687f362a3_640.jpg',false,'2022/06/17',3,3);
+INSERT INTO tutorials(name,target,explanation,image, published,creation_date,quizz_id,formation_id) 
+VALUES ('Tuto 4','Aenean vitae mauris magna. Pellentesque ultrices nisl orci, eget viverra tellus pharetra vulputate. Quisque dictum.','Explanation1','https://randompicturegenerator.com/img/dragon-generator/g9b72abb3388f87375d88f2c3cb2fa279a0fe380ab82edac115301d9331dfe3b7c3e453d72e99ba931de26c3f1540df1e_640.jpg',true,'2023/04/14',1,1);
+INSERT INTO tutorials(name,target,explanation,image, published,creation_date,quizz_id,formation_id) VALUES ('Tuto 5','Ut dignissim at nulla vel rhoncus. Sed vitae felis sit amet quam consectetur congue. Morbi','Explanation2','https://randompicturegenerator.com/img/dragon-generator/g9d319c882c20258e422f2e608f9cb4f132faccc2c5d0d8f642b331e75e59815d87c2cba770a8ce77fce4afdacaacc782_640.jpg',true,'2023/04/05',2,2);
+INSERT INTO tutorials(name,target,explanation,image,published,creation_date,quizz_id,formation_id) VALUES ('Tuto 6','Etiam ut sapien quam. Proin lacus leo, elementum in volutpat at, pulvinar sit amet urna.','Explanation3','https://randompicturegenerator.com/img/dragon-generator/g43a4ef235a57ff339250b27e486be537454a43ec5fb652c2cdc99754019d679e232a4785776983bc39a755e687f362a3_640.jpg',false,'2022/06/17',3,3);
 
 
 INSERT INTO validated_by_user(user_id,tutorial_id) VALUES (1,2);
 INSERT INTO validated_by_user(user_id,tutorial_id) VALUES (3,2);
 INSERT INTO validated_by_user(user_id,tutorial_id) VALUES (2,3);
+
+INSERT INTO resources(name,content,tutorial_id) 
+VALUES ('Ressource 1','Pellentesque dolor magna, interdum et vulputate quis, vulputate sed neque. Proin luctus in risus vitae faucibus. Curabitur volutpat et urna vel accumsan. Ut gravida est non quam maximus mattis. Donec urna elit, elementum eu ligula quis, lobortis faucibus arcu. Cras sem purus, sagittis id neque vel, accumsan posuere arcu. Suspendisse rhoncus tellus id nisl vehicula, in pellentesque tortor sodales. Vestibulum ultricies bibendum pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla auctor metus et diam pulvinar ullamcorper. Curabitur efficitur sapien elit, sed vulputate sem scelerisque consectetur. Curabitur ut bibendum sem.',3);
+INSERT INTO resources(name,content,tutorial_id) VALUES ('Ressource 2','Duis justo orci, pellentesque quis ante at, congue hendrerit justo. Donec luctus, enim quis ultricies interdum, risus tellus hendrerit nisl, a luctus arcu eros sit amet nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas eget mauris a magna auctor congue feugiat vel sem. Nulla facilisi. Proin sit amet fringilla purus. Duis sed aliquam nisl. Nulla feugiat nibh quis turpis commodo accumsan. Duis aliquam quam vel lorem dictum suscipit. Duis massa metus, interdum eu dui sit amet, lobortis consequat nulla.',2);
+INSERT INTO resources(name,content,tutorial_id) VALUES ('Ressource 3','Curabitur bibendum finibus pellentesque. Nunc sollicitudin vitae neque ullamcorper finibus. Nullam rutrum sed ante sed fringilla. Pellentesque sagittis fermentum aliquet. Maecenas pulvinar ipsum risus, a consectetur mauris ornare vel. Curabitur eget iaculis elit. Maecenas cursus justo at lobortis eleifend. Fusce vitae lorem venenatis, varius nunc eu, tincidunt justo. Curabitur a lectus laoreet, posuere nulla eu, malesuada mauris. Curabitur at mi tortor.',1);
+
+
+INSERT INTO questions(content,quizz_id) VALUES ('Question 1',2);
+INSERT INTO questions(content,quizz_id) VALUES ('Question 2',1);
+INSERT INTO questions(content,quizz_id) VALUES ('Question 3',3);
+INSERT INTO questions(content,quizz_id) VALUES ('Question 4',1);
+
+INSERT INTO questions(content,quizz_id) VALUES ('Question 5',2);
+INSERT INTO questions(content,quizz_id) VALUES ('Question 6',1);
+INSERT INTO questions(content,quizz_id) VALUES ('Question 7',3);
+INSERT INTO questions(content,quizz_id) VALUES ('Question 8',1);
+
+INSERT INTO answers(content,question_id) VALUES ('Reponse 1',1);
+INSERT INTO answers(content,question_id) VALUES ('Reponse 2',2);
+INSERT INTO answers(content,question_id) VALUES ('Reponse 3',3);
+INSERT INTO answers(content,question_id) VALUES ('Reponse 4',4);
+INSERT INTO answers(content,question_id) VALUES ('Reponse 5',5);
+INSERT INTO answers(content,question_id) VALUES ('Reponse 6',6);
+INSERT INTO answers(content,question_id) VALUES ('Reponse 7',7);
+INSERT INTO answers(content,question_id) VALUES ('Reponse 8',8);
