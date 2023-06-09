cf9ee14fc559e5f542e45965380dd57b06d3baf2
Cyrielle Thomas <cyriellethomas@Cyrielle.local>
2023-06-09 11:09:51 +0200

    Route get tuto coté admin pour récupérer les données de la table tutorials uniquement

---
+++ backend/src/controllers/TutoAdminControllers.js
@@ -0,0 +1,86 @@
+const models = require("../models");
+
+const browse = (req, res) => {
+  models.TutoAdmin.findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  models.TutoAdmin.find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const TutoAdmin = req.body;
+
+  // TODO validations (length, format...)
+
+  TutoAdmin.id = parseInt(req.params.id, 10);
+
+  models.TutoAdmin.update(TutoAdmin)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const TutoAdmin = req.body;
+
+  // TODO validations (length, format...)
+
+  models.TutoAdmin.insert(TutoAdmin)
+    .then(([result]) => {
+      res.location(`/admin/tuto/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.TutoAdmin.delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  read,
+  edit,
+  add,
+  destroy,
+};
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+// const itemControllers = require("./controllers/itemControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+// router.get("/items", itemControllers.browse);
+// router.get("/items/:id", itemControllers.read);
+// router.put("/items/:id", itemControllers.edit);
+// router.post("/items", itemControllers.add);
+// router.delete("/items/:id", itemControllers.destroy);
+
+const TutoAdminControllers = require("./controllers/TutoAdminControllers");
+
+router.get("/admin/tuto", TutoAdminControllers.browse);
+router.get("/admin/tuto/:id", TutoAdminControllers.read);
+router.put("/admin/tuto/:id", TutoAdminControllers.edit);
+router.post("/admin/tuto", TutoAdminControllers.add);
+router.delete("/admin/tuto/:id", TutoAdminControllers.destroy);
 
 module.exports = router;
