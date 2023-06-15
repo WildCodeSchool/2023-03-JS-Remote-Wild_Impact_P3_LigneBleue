7947f03f01c644a4964e6e2e902604eedc8ed3fb
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-06-15 14:49:20 +0200

    fetch et map des formations de la page home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const [formations, setFormations] = useState([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/formations`)
+      .then((res) => res.json())
+      .then((form) => setFormations(form))
+      .catch((err) => console.error(err));
+  }, []);
+  return (
+    <section>
+      <div className="flex flex-row">
+        {formations.map((formation) => (
+          <>
+            <h3>{formation.title}</h3>
+            <img
+              key={formation.id}
+              src={formation.icon}
+              alt="icon de la formation"
+            />
+          </>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Home;
