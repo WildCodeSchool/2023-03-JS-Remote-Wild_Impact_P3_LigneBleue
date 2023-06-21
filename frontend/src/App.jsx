a28c020ce108e0b19dcdf6043b1422d4e34c57ab
adrien141094 <123838260+adrien141094@users.noreply.github.com>
2023-06-21 15:46:59 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Connexion from "./pages/Connexion";
 import Home from "./pages/Home";
-import Test from "./pages/Test";
-import Test2 from "./pages/Test2";
 import Header from "./components/Header";
 import Breadcrumbs from "./components/Breadcrumbs";
 
@@ -10,18 +7,12 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
         <Header />
         <Breadcrumbs />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/test/test" element={<Test2 />} />
-        </Routes>
+        <Home />
         <Connexion />
       </div>
-    </BrowserRouter>
   );
 }
 
