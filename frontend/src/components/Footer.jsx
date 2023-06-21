e5ce2d4ba89c9ed627e0bae444ef5b6fc3555111
Cyrielle Thomas <cyriellethomas@Cyrielle.local>
2023-06-21 21:25:02 +0200

    Footer avec Tailwind

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,11 +3,11 @@ import userIcon from "../assets/userIcon.png";
 
 function Footer() {
   return (
-    <div className="flex flex-row justify-center items-center m-4 p-4 bg-yellow rounded-tr-3xl rounded-tl-3xl">
-      <img className="w-14 h-14 " src={userIcon} alt="User icon" />
-      <h3 className="text-center  text-blue font-bold">
+    <div className="flex flex-row  items-center justify-center  p-4 bg-yellow rounded-tr-full rounded-tl-full">
+      <img className="w-14 h-14 items-center " src={userIcon} alt="User icon" />
+      <div className="text-xl pl-6 w-auto text-center  text-blue font-bold">
         Accéder à mon parcours
-      </h3>
+      </div>
     </div>
   );
 }
