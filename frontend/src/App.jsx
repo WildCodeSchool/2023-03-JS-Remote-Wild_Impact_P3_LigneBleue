fa9901a5518c69292bec167280c32ce7156f82b7
Jumzer <bluevictor@hotmail.fr>
2023-06-21 15:37:05 +0200

    sur home : affichage header et retrait composant connexion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
-import Connexion from "./pages/Connexion";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
-
-      <Connexion />
     </div>
   );
 }
