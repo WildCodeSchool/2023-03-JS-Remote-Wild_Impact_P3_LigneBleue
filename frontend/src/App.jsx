4a971659fe22bc3decaaf0f563e5e7f60d92dee9
Cyrielle Thomas <cyriellethomas@Cyrielle.local>
2023-06-22 17:14:52 +0200

    Ajout route et mise à jour des liens vers les composants

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
-import Footerbis from "./components/Footerbis";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserLayout from "./pages/Layout/UserLayout";
+import OneTuto from "./pages/Users/OneTuto";
+import AdminLayout from "./pages/Layout/AdminLayout";
+import TutoAdmin from "./pages/Admin/TutoAdmin";
+import Footer from "./components/Footerbis";
 import Connexion from "./pages/Connexion";
-import Home from "./pages/Home";
+import Home from "./pages/Users/Home";
 import Header from "./components/Header";
 import Breadcrumbs from "./components/Breadcrumbs";
+import Erreur from "./pages/Erreur";
 
 import "./App.css";
 
@@ -13,12 +18,22 @@ function App() {
       <Header />
       <BrowserRouter>
         <Breadcrumbs />
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<UserLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="tutorials/:id" element={<OneTuto />} />
+              <Route path="/connexion" element={<Connexion />} />
+              <Route path="/*" element={<Erreur />} />
+            </Route>
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route path="tuto" element={<TutoAdmin />} />
+            </Route>
+          </Routes>
+        </div>
       </BrowserRouter>
-      <Home />
-      <Connexion />
-      <Footerbis />
+      <Footer />
     </div>
   );
 }
-
 export default App;
