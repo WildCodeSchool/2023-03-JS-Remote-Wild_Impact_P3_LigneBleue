979dfb23eaae0607acf885fc787911848bfc4aee
Laure Graves <graveslaure@gmail.com>
2023-06-23 11:35:22 +0200

    controle de la recuperation des données d'une liste de tutos entre le back et le front

--- frontend/src/pages/Users/TutorialsList.jsx
+++ frontend/src/pages/Users/TutorialsList.jsx
@@ -1,26 +1,27 @@
 import React, { useState, useEffect } from "react";
-import TutorialsCard from "../../components/TutorialsCard";
+import { useParams } from "react-router-dom";
+// import TutorialsCard from "../../components/TutorialsCard";
 
 function TutorialsList() {
-  const [tutorials, setTutorials] = useState([]);
-
+  const [List, setList] = useState([]);
+  const { id } = useParams();
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/formations`)
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/formations/${id}`)
       .then((res) => res.json())
-      .then((tuto) => setTutorials(tuto))
+      .then((data) => setList(data))
       .catch((err) => console.error(err));
   }, []);
 
   return (
     <>
-      <div>Coucou</div>
-      <section>
+      <div>{List}</div>
+      {/* <section>
         <div className="flex flex-row">
           {tutorials.map((tutorial) => (
             <TutorialsCard tutorial={tutorial} />
           ))}
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
