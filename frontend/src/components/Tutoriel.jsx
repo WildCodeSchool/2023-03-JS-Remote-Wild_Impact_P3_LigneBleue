f77f38fe6751842151f8bc85f6a433b49e1c0f5a
Jumzer <bluevictor@hotmail.fr>
2023-07-05 12:22:31 +0200

    début composant tuto

--- frontend/src/components/Tutoriel.jsx
+++ frontend/src/components/Tutoriel.jsx
@@ -3,24 +3,26 @@ import img from "../assets/Tutoriel.jpg";
 
 function Tutoriel() {
   return (
-    <div>
+    <div className="">
       <h1 className="p-4 flex justify-center">Nom du tutoriel</h1>
-
-      <div className="bg-champagne rounded-tr-3xl rounded-tl-3xl flex justify-center flex-col m-4">
-        <h2 className="p-4 flex justify-center">Objectif du tutoriel</h2>
-        <img
-          className="p-4 object-center object-contain h-300 w-96"
-          src={img}
-          alt=""
-        />
-        <p className="text-center p-8">
+      <div className="flex flex-col items-center bg-champagne rounded-3xl m-4">
+        <h2 className="p-4">Objectif du tutoriel</h2>
+        <img className="rounded-3xl p-4 h-300 w-96" src={img} alt="" />
+        <p className="p-8 text-center">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus
           molestiae nostrum ullam aut laboriosam corrupti maiores nemo eius
           asperiores! Maxime sit, corrupti necessitatibus sequi laboriosam nemo
           nam illum animi?
         </p>
-        <div className="bg-white rounded-tr-3xl rounded-tl-3xl flex justify-center m-12>">
-          <h2>Validez votre tuto</h2>
+        <div className="bg-white rounded-3xl m-8 >">
+          <h2 className="p-12 ">Validez votre tuto</h2>
+          <h3>Question 1</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
+            adipisci consectetur dolor ipsa totam perspiciatis at ea. Adipisci,
+            nam eos itaque officia, ipsa vitae dicta tempora eius magni ducimus
+            quo.
+          </p>
         </div>
       </div>
     </div>
