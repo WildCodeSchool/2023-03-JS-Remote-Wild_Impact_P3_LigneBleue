8078dae5b1de8596653eade5850883083f6cd004
Laure Graves <graveslaure@gmail.com>
2023-07-05 14:20:05 +0200

    début de mise en page tuto

--- frontend/src/pages/Users/TutorialsList.jsx
+++ frontend/src/pages/Users/TutorialsList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import TutorialsCard from "../../components/TutorialsCard";
+// import TutorialsCard from "../../components/TutorialsCard";
 
 function TutorialsList() {
   const [List, SetList] = useState([]);
@@ -16,10 +16,18 @@ function TutorialsList() {
     <>
       {/* <div>{List}</div> */}
       <section>
-        <div className="flex flex-row">
-          {List.map((tutorial) => (
-            <TutorialsCard key={tutorial.id} tutorial={tutorial} />
-          ))}
+        <div className="pt-10 pr-4 pl-4">
+          <div className="bg-champagne grid grid-cols-3 rounded-tr-3xl rounded-tl-3xl">
+            {List.map((tutorial) => (
+              <div
+                key={tutorial.id}
+                className="formations bg-light_blue px-3 py-3 my-10 mx-10"
+              >
+                <h3 className="text-2xl text-center pt-2">{tutorial.name}</h3>
+                <div className="flex justify-center" />
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </>
