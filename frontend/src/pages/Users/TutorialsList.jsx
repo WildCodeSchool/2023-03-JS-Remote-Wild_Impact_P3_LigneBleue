a04f710c065c5477c789d1fb3706455bf36edc07
Cyrielle Thomas <cyriellethomas@Cyrielle.local>
2023-07-07 12:07:21 +0200

    Footer bis sur les pages formations , tutos et avancement parcours

--- frontend/src/pages/Users/TutorialsList.jsx
+++ frontend/src/pages/Users/TutorialsList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Icons from "../../components/Icons";
 import connexion from "../../services/connexion";
+import Footerbis from "../../components/Footerbis";
 
 function TutorialsList() {
   const [list, SetList] = useState([]);
@@ -20,24 +21,27 @@ function TutorialsList() {
   }, []);
 
   return (
-    <section>
-      <div className="py-5 pr-4 pl-4 mb-1">
-        {" "}
-        <div className="bg-champagne grid grid-cols-3 max-sm:grid max-sm:grid-cols-2 place-items-center rounded-3xl">
-          {list.map((tutorial) => (
-            <div
-              key={tutorial.id}
-              className="formations flex flex-col justify-center items-center bg-brown_light px-3 py-3 my-10 mx-10 w-52 h-40"
-            >
-              <h3 className="text-2xl text-center pt-2">{tutorial.name}</h3>
-              <div className="flex justify-center">
-                <Icons icon={tutorial.icon} />
+    <>
+      <section>
+        <div className="py-5 pr-4 pl-4 mb-1">
+          {" "}
+          <div className="bg-champagne grid grid-cols-3 max-sm:grid max-sm:grid-cols-2 place-items-center rounded-3xl">
+            {list.map((tutorial) => (
+              <div
+                key={tutorial.id}
+                className="formations flex flex-col justify-center items-center bg-brown_light px-3 py-3 my-10 mx-10 w-52 h-40"
+              >
+                <h3 className="text-2xl text-center pt-2">{tutorial.name}</h3>
+                <div className="flex justify-center">
+                  <Icons icon={tutorial.icon} />
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Footerbis />
+    </>
   );
 }
 
