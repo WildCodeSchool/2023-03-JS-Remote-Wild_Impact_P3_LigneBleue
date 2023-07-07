56b85ec2c717434bdde6c016eb507452aba678d3
Jumzer <bluevictor@hotmail.fr>
2023-07-07 10:06:49 +0200

    front envoi info ok

--- frontend/src/pages/Inscription.jsx
+++ frontend/src/pages/Inscription.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import connexion from "../services/connexion";
 
 function Signin() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleUser = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+
+  const login = async (event) => {
+    event.preventDefault();
+    try {
+      const log = await connexion.post("/signin", user);
+      console.info(log);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex justify-center flex-col p-4">
       <div className="p-4 flex justify-center">
         <h1>M'inscrire pour accéder à mon parcours</h1>
       </div>
       <div className="flex justify-center">
-        <form className=" shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form
+          className=" shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={(event) => login(event)}
+        >
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2 flex justify-center"
@@ -17,9 +40,14 @@ function Signin() {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
+              id="email"
+              value={user.email}
+              type="email"
+              name="email"
               placeholder="E-mail"
+              required
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+              onChange={(event) => handleUser(event)}
             />
           </div>
           <div className="mb-6">
@@ -33,7 +61,11 @@ function Signin() {
               className="shadow border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
+              name="password"
+              value={user.password}
+              onChange={(event) => handleUser(event)}
               placeholder="******************"
+              required
             />
           </div>
           <div className="flex flex-col">
