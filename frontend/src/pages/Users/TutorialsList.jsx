9ba295030da7b5b4aafbf216fe6455019b869603
Cyrielle Thomas <118595509+CyrThoDev@users.noreply.github.com>
2023-07-07 09:26:11 +0200

    Update TutorialsList.jsx

--- frontend/src/pages/Users/TutorialsList.jsx
+++ frontend/src/pages/Users/TutorialsList.jsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import Icons from "../../components/Icons";
 import connexion from "../../services/connexion";
 
-
 function TutorialsList() {
   const [list, SetList] = useState([]);
   const { id } = useParams();
@@ -17,28 +16,27 @@ function TutorialsList() {
   };
 
   useEffect(() => {
-      getTutorialsList();
+    getTutorialsList();
   }, []);
 
-
   return (
-      <section>
-        <div className="pt-10 pr-4 pl-4">
-          {" "}
-          <div className="bg-champagne grid grid-cols-3 rounded-tr-3xl rounded-tl-3xl">
-            {list.map((tutorial) => (
-              <div
-                key={tutorial.id}
-                className="formations bg-light_blue px-3 py-3 my-10 mx-10"
-              >
-                <h3 className="text-2xl text-center pt-2">{tutorial.name}</h3>
-                <div className="flex justify-center">
-                  <Icons icon={tutorial.icon} />
-                </div>
+    <section>
+      <div className="pt-10 pr-4 pl-4">
+        {" "}
+        <div className="bg-champagne grid grid-cols-3 rounded-tr-3xl rounded-tl-3xl">
+          {list.map((tutorial) => (
+            <div
+              key={tutorial.id}
+              className="formations bg-light_blue px-3 py-3 my-10 mx-10"
+            >
+              <h3 className="text-2xl text-center pt-2">{tutorial.name}</h3>
+              <div className="flex justify-center">
+                <Icons icon={tutorial.icon} />
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
+      </div>
     </section>
   );
 }
