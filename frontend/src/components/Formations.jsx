2afd40448d7df95390567c76169a1ccd044a01a4
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-07-12 10:56:08 +0200

    ajout deuxieme bouton accueil

--- frontend/src/components/Formations.jsx
+++ frontend/src/components/Formations.jsx
@@ -35,7 +35,20 @@ function Formations() {
               </div>
             </button>
           </Link>
+          <div className="bg-champagne grid grid-cols-3 place-items-center rounded-tr-3xl rounded-tl-3xl">
+            <Link to="/aide">
+              <button
+                type="button"
+                className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400"
+              >
+                <div className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  <h3 className="text-xl text-center pt-2">Se faire aider</h3>
+                </div>
+              </button>
+            </Link>
+          </div>
         </div>
+
         <div>
           <div className="bg-champagne grid grid-cols-3 max-sm:grid max-sm:grid-cols-2 place-items-center rounded-br-3xl rounded-bl-3xl">
             {formations.slice(0, 9).map((formation) => (
