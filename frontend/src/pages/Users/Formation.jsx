d87689094ebcde71b6d53f4d91b0532d6374b173
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-07-17 14:29:59 +0200

    fix page formation

--- frontend/src/pages/Users/Formation.jsx
+++ frontend/src/pages/Users/Formation.jsx
@@ -4,6 +4,7 @@ import Icons from "../../components/Icons";
 import connexion from "../../services/connexion";
 import Footerbis from "../../components/Footerbis";
 import NoContent from "../../components/NoContent";
+import "./Formation.css";
 
 function TutorialsList() {
   const [list, setList] = useState([]);
@@ -26,15 +27,15 @@ function TutorialsList() {
     <>
       <section>
         <div className="py-5 pr-4 pl-4 mb-1">
-          <div className="bg-champagne grid grid-cols-3 max-sm:grid max-sm:grid-cols-2 place-items-center rounded-3xl">
+          <div className="bg-champagne grid grid-cols-3 max-sm:grid max-sm:grid-cols-2 place-items-center rounded-br-3xl rounded-bl-3xl">
             {list.length > 0 &&
               list.map((tutorial) => (
                 <div
                   key={tutorial.id}
-                  className="formations flex flex-col justify-center items-center bg-brown_light px-3 py-3 my-10 mx-10 w-52 h-40 max-sm:w-36 max-sm:h-30"
+                  className="formation flex flex-col justify-center items-center bg-brown_light px-3 py-3 my-10 mx-10 max-sm:w-36 max-sm:h-30 w-52 h-40 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
                 >
                   <Link to={`/formations/${fid}/tutoriel/${tutorial.id}`}>
-                    <h3 className="text-2xl text-center max-sm:text-sm pt-2">
+                    <h3 className="text-xl max-sm:text-sm text-center">
                       {tutorial.name}
                     </h3>
                     <div className="flex justify-center">
