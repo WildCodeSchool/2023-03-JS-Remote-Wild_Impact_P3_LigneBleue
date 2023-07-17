2fdff8e95191e57c45631904ff244737f78e345c
ADRIEN TURCEK_PERRON <adrien.turcekperron@gmail.com>
2023-07-17 15:40:29 +0200

    mise au propre du code front + affichage donnée quizz

--- frontend/src/components/QuizzTab.jsx
+++ frontend/src/components/QuizzTab.jsx
@@ -1,58 +1,77 @@
-function QuizzTab() {
+import React, { useEffect, useState } from "react";
+import connexion from "../services/connexion";
+
+function QuizzTab({ tutorialId }) {
+  const [quizz, setQuizz] = useState();
+
+  const handleQuizz = (name, value) => {
+    setQuizz({ ...quizz, [name]: value });
+  };
+
+  // const updateQuizzState = (id) => {
+  //   if (id === "") {
+  //     setQuizzId(quizzModel);
+  //   } else {
+  //     setQuizzId(quizz.find((art) => art.id === +id));
+  //   }
+  // };
+
+  const getQuizz = async () => {
+    try {
+      const oneQuizz = await connexion.get(`/quizz/${tutorialId}`);
+      setQuizz(oneQuizz);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getQuizz();
+  }, []);
+
   return (
     <div>
-      <div className="relative z-0 w-full mb-6 group pb-4 mt-4">
-        <div className="flex">
-          <input
-            type="url"
-            className="mr-4 block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-500 peer"
-            placeholder="Nom de la Question"
-            required
-          />
-        </div>
-
-        <label
-          htmlFor="floating_email"
-          className="peer-focus:font-medium absolute  text-lg text-gray-500 duration-300 transform -translate-y-6 scale-75  top-0.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-        >
-          Question
-        </label>
-      </div>
-      <div className="relative z-0 w-full mb-6 group pb-4 mt-4">
-        <div className="flex flex-col">
-          <input
-            type="url"
-            className="mr-4 block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-500 peer"
-            placeholder="1er choix de réponse"
-            required
-          />
-          <input
-            type="url"
-            className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-500 peer"
-            placeholder="2ème choix de réponse"
-            required
-          />
-          <input
-            type="url"
-            className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-500 peer"
-            placeholder="3ème choix de réponse"
-            required
-          />
-          <input
-            type="url"
-            className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-500 peer"
-            placeholder="4ème choix de réponse"
-            required
-          />
-        </div>
-
-        <label
-          htmlFor="floating_email"
-          className="peer-focus:font-medium absolute  text-lg text-gray-500 duration-300 transform -translate-y-6 scale-75  top-0.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-        >
-          Réponses
-        </label>
-      </div>
+      {quizz && (
+        <>
+          <h3>{quizz.title}</h3>
+          {quizz.questions.length > 0 &&
+            quizz.questions.map((quest, index) => (
+              <details className="w-full mb-6 pb-4 mt-4">
+                <summary className="flex">
+                  <label className="text-lg text-gray-500 ">
+                    Question {index + 1}
+                    <input
+                      type="text"
+                      className="mr-4 py-2.5 px-0 w-full text-sm text-black border-0 border-b-2 border-gray-200"
+                      placeholder="Nom de la Question"
+                      value={quest.content}
+                      name="questions"
+                      onChange={(event) =>
+                        handleQuizz(event.target.name, event.target.value)
+                      }
+                      required
+                    />
+                  </label>
+                </summary>
+                <div className="w-full mb-6 pb-4 mt-4">
+                  <div className="flex flex-col">
+                    {quest.answers.map((answer, jindex) => (
+                      <label className="text-lg text-gray-500">
+                        Réponses {jindex + 1}
+                        <input
+                          type="text"
+                          className="mr-4 block py-2.5 px-0 w-full text-sm text-black border-0 border-b-2 border-gray-200"
+                          required
+                          value={answer.answers}
+                        />
+                      </label>
+                    ))}
+                  </div>
+                </div>
+              </details>
+            ))}
+        </>
+      )}
     </div>
   );
 }
