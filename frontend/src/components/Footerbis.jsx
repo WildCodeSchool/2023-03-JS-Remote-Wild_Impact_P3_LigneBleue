ea11b78598bac5f3ced160e160e563690078a483
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-07-27 15:13:38 +0200

    accueil ok

--- frontend/src/components/Footerbis.jsx
+++ frontend/src/components/Footerbis.jsx
@@ -5,25 +5,25 @@ import userIcon from "../assets/userIcon.png";
 function Footerbis() {
   return (
     <div className="flex flex-row justify-between ">
-      <div className="flex items-center h-20 w-72 pl-16 bg-yellow rounded-tr-full max-sm:w-36">
-        <div className="pr-3">
-          <img className="w-10 min-sm:h-10" src={userIcon} alt="User icon" />
+      <div className="flex items-center justify-center h-20 w-72 bg-yellow rounded-tr-full max-sm:w-40 max-sm:grid max-sm:grid-cols-2">
+        <div className="max-sm:pr-3 pr-3 flex justify-center max-sm:justify-self-end">
+          <img className="w-10" src={userIcon} alt="User icon" />
         </div>
         <div className="pt-3">
           <Link to="/">
-            <div className="text-base text-center min-sm:pb-3 text-blue font-bold min-sm:text-sm">
+            <div className="text-base text-blue font-bold max-sm:text-xs max-sm:justify-self-start">
               Accueil
             </div>
           </Link>
         </div>
       </div>
-      <div className="flex justify-center items-center h-20 w-72  bg-yellow rounded-tl-full max-sm:w-36">
+      <div className="flex justify-center items-center h-20 w-72  bg-yellow rounded-tl-full max-sm:w-40 max-sm:grid max-sm:grid-cols-2">
         <div className="max-sm:pt-3 pr-3">
-          <img className="w-10 min-sm:h-10" src={userIcon} alt="User icon" />
+          <img className="w-10" src={userIcon} alt="User icon" />
         </div>
         <div className="pt-2">
           <Link to="/connexion">
-            <div className="flex text-base text-center min-sm:pb-3 text-blue font-bold min-sm:text-sm w-32">
+            <div className="flex text-base text-center text-blue font-bold max-sm:text-xs w-32">
               Accéder à mon parcours
             </div>
           </Link>
